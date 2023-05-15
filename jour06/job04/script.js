f28a09a7be4ba55bbb2ee8fc16eac170b3600193
marwan-rouissi <115158061+marwan-rouissi@users.noreply.github.com>
2023-05-15 20:09:47 +0200

    Add job04

---
+++ jour06/job04/script.js
@@ -0,0 +1,25 @@
+// window.addEventListener("resize" , function() {
+//     var L = document.getElementById("L");
+    
+//     if (window.matchMedia("(max-width: 1280px)").matches) {
+
+//         L.href = "style4.css";
+//       } 
+//     if (window.matchMedia("screen and (min-width: 1281px) and (max-width: 1679px)").matches) {
+
+//         L.href = "style3.css";
+//       }
+//     if (window.matchMedia("screen and (min-width: 1681px) and (max-width: 1920px)").matches) {
+
+//         L.href = "style2.css";
+//       }
+
+// })
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu")
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+})
